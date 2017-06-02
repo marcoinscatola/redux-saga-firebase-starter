24069298261275c90f8cae500320f4ba3a1c4a27
Marco <lepore86@gmail.com>
2017-06-02 22:18:46 +0200

    Add firebase wrapper for auth methods

---
+++ src/api/auth.js
@@ -0,0 +1,78 @@
+import {firebaseAuth} from 'api/firebase';
+
+/**
+ * Return the current logged in user or null
+ */
+export function getFirebaseCurrentUser() {
+    return firebaseAuth.currentUser;
+}
+
+/**
+ * Subscribe to the onAuthStateChanged event and returns a promise that will
+ * resolve the next time the auth state changes. Useful to get the auth status
+ * at initialization time since firebase will asynchronously trigger the event
+ * with the previously logged-in user (or null) after initialization
+ */
+export function checkAuthState() {
+    return new Promise((resolve, reject) => {
+        const unsubscribe = firebaseAuth.onAuthStateChanged(
+            user => {
+                resolve(user);
+                unsubscribe();
+            },
+            err => {
+                reject(err);
+                unsubscribe();
+            },
+        )
+    })
+
+}
+
+/**
+ * Attempt a user sign up with the provided email and password and returns a
+ * promise with the user data, if successful 
+ */
+export function firebaseSignupEmail(email, password) {
+    return new Promise((resolve, reject) => {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+        .catch(error => {
+            reject(`${error.code} - ${error.message}`)
+        })
+        .then(res => {
+            resolve(firebaseAuth.currentUser)
+        })
+    })
+}
+
+/**
+ * Attempt a user log in with the provided email and password and returns a
+ * promise with the user data, if successful
+ */
+export function firebaseLoginEmail(email, password) {
+    return new Promise((resolve, reject) => {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+        .catch(error => {
+            reject(`${error.code} - ${error.message}`)
+        })
+        .then(res => {
+            resolve(firebaseAuth.currentUser)
+        })
+    })
+}
+
+
+/**
+ * Attempt a user logout and returns a promise
+ */
+export function firebaseLogout() {
+    return new Promise((resolve, reject) => {
+        firebaseAuth.signOut()
+        .catch(error => {
+            reject(`${error.code} - ${error.message}`)
+        })
+        .then(res => {
+            resolve(res)
+        })
+    })
+}
---
+++ src/api/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+/**
+ * Config your firebase parameters in a .env.development.local file or a
+ * .env.production.local file with the following variables.
+ * Make sure their names start with REACT_APP_ otherwise they'll 
+ * be filtered out during the build step by the create-react-app scripts
+ */
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+export const firebaseApp = firebase.initializeApp(config);
+export const firebaseAuth = firebaseApp.auth();
