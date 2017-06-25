8aeaaea39e51bc250f1aa67c669e0530df8c1e29
Marco Lepore <lepore86@gmail.com>
2017-06-25 09:41:35 +0200

    Setup redux-dev-tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,25 @@
 import {createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import sagas from './sagas';
+
+const composeEnhancers = composeWithDevTools({
+    shouldHotReload:false,
+    features: {
+        persist: false
+    }
+})
+
 
 export const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware();
     const store = createStore(
         reducers,
-        applyMiddleware(sagaMiddleware)
+        {}, // preloaded state
+        composeEnhancers(
+            applyMiddleware(sagaMiddleware)
+        )
     );
     sagaMiddleware.run(sagas);
     return store;
